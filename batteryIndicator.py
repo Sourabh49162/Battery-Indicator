edec16df4386bb1edaa562e5d7a4bab3491a4123
Sourabh <sourabh@adzinymedia.com>
2020-12-08 21:30:50 +0530

    Minor Changes

--- batteryIndicator.py
+++ batteryIndicator.py
@@ -2,6 +2,7 @@ import os
 import psutil 
 from gtts import gTTS 
 from time import sleep
+from subprocess import call
 
  
 
@@ -11,14 +12,22 @@ if __name__ == "__main__":
     if(((battery.percent) < 11) and ((battery.power_plugged) == False)):
         plugInText = f"Only {battery.percent} percent battery is available. Please connect to charger immediately."
         myobj = gTTS(text=plugInText, lang=language, slow=False) 
+        volume = 100
+        call(["amixer", "-D", "pulse", "sset", "Master", str(volume)+"%"])
         myobj.save("plugInText.mp3")
         os.system("mpg321 plugInText.mp3") 
         sleep(10)
+        volume = 0
+        call(["amixer", "-D", "pulse", "sset", "Master", str(volume)+"%"])
         os.remove("plugInText.mp3")
     elif(((battery.percent) == 100) and ((battery.power_plugged) == True)):
         plugOutText = f"Battery fully charged. Please remove the charger immediately."
         myobj = gTTS(text=plugOutText, lang=language, slow=False) 
+        volume = 100
+        call(["amixer", "-D", "pulse", "sset", "Master", str(volume)+"%"])
         myobj.save("plugOutText.mp3")
         os.system("mpg321 plugOutText.mp3") 
         sleep(10)
+        volume = 0
+        call(["amixer", "-D", "pulse", "sset", "Master", str(volume)+"%"])
         os.remove("plugOutText.mp3")
